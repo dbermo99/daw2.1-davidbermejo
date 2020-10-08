8d1c61217aaa0d7ad1d7db9df1db48a5d8e5a10b
dbermo99 <72387622+dbermo99@users.noreply.github.com>
2020-10-08 12:20:39 +0200

    añadir ejercicio

---
+++ EjerciciosDavidBermejoMoreno.php
@@ -0,0 +1,16 @@
+<?php
+if(5<10)
+    echo "verdadero</br>";
+else
+    echo "falso";
+
+for ($i=1; $i<=10; $i++) {
+    echo $i;
+}
+echo "</br>";
+$condicion= true;
+if($condicion)
+    echo "Hola";
+else
+    echo "Adios";
+?>
