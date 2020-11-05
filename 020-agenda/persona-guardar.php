6e2cad244c5bc24d88a8f548a323c5582f19b7ff
David Bermejo <david.bermejo13@educa.madrid.org>
2020-11-05 23:06:11 +0100

    Ejercicio Agenda Persona Estrellas

---
+++ 020-agenda/persona-guardar.php
@@ -0,0 +1,80 @@
+<?php
+	require_once "_varios.php";
+
+	$pdo = obtenerPdoConexionBD();
+
+	$id = (int)$_REQUEST["id"];
+	if(isset($_REQUEST["estrella"]))
+	    $estrella= $_REQUEST["estrella"];
+	else
+	    $estrella= null;
+	$nombre = $_REQUEST["nombre"];
+	$apellido= $_REQUEST["apellido"];
+	$telefono= $_REQUEST["telefono"];
+	$personaCategoriaId= $_REQUEST["categoriaId"];
+
+	$nuevaEntrada = ($id == -1);
+	
+	if ($nuevaEntrada) {
+ 		$sql = "INSERT INTO persona (estrella, nombre, apellido, telefono, categoria_id) VALUES (?, ?, ?, ?)";
+ 		$parametros = [$estrella, $nombre, $apellido, $telefono, $personaCategoriaId];
+	} else {
+ 		$sql = "UPDATE persona SET estrella=?, nombre=?, apellido=?, telefono=?, categoria_id=? WHERE id=?";
+        $parametros = [$estrella, $nombre, $apellido, $telefono, $personaCategoriaId, $id];
+ 	}
+ 	
+    $sentencia = $pdo->prepare($sql);
+    $sql_con_exito = $sentencia->execute($parametros);
+
+ 	$una_fila_afectada = ($sentencia->rowCount() == 1);
+ 	$ninguna_fila_afectada = ($sentencia->rowCount() == 0);
+
+ 	$correcto = ($sql_con_exito && $una_fila_afectada);
+
+ 	$datos_no_modificados = ($sql_con_exito && $ninguna_fila_afectada);
+?>
+
+
+
+<html>
+
+<head>
+	<meta charset="UTF-8">
+</head>
+
+
+
+<body>
+
+<?php
+	if ($correcto || $datos_no_modificados) { ?>
+
+		<?php if ($id == -1) { ?>
+			<h1>Inserción completada</h1>
+			<p>Se ha insertado correctamente la nueva entrada de <?php echo $nombre; ?>.</p>
+		<?php } else { ?>
+			<h1>Guardado completado</h1>
+			<p>Se han guardado correctamente los datos de <?php echo $nombre; ?>.</p>
+
+			<?php if ($datos_no_modificados) { ?>
+				<p>No ha habido ninguna modificacion.</p>
+			<?php } ?>
+		<?php }
+?>
+
+<?php
+	} else {
+?>
+
+	<h1>Error en la modificación.</h1>
+	<p>No se han podido guardar los datos de la persona.</p>
+
+<?php
+	}
+?>
+
+<a href="persona-listado.php">Volver al listado de persona.</a>
+
+</body>
+
+</html>
---
+++ 020-agenda/persona-listado.php
@@ -0,0 +1,86 @@
+<?php
+
+    require_once "_varios.php";
+
+    $pdo = obtenerPdoConexionBD();
+
+    $sql = "
+           SELECT
+                p.id     AS p_id,
+                p.estrella AS p_estrella,
+                p.nombre AS p_nombre,
+                p.nombre AS p_nombre,
+                p.apellido AS p_apellido,
+                p.telefono AS p_telefono,
+                p.categoria_id AS p_categoriaId,
+                c.id     AS c_id,
+                c.nombre AS c_nombre
+            FROM
+               persona p, categoria c
+               WHERE p.categoria_id = c.id
+            ORDER BY p.nombre
+    ";
+
+    $select = $pdo->prepare($sql);
+    $select->execute([]);
+    $rs = $select->fetchAll();
+?>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+
+
+<body>
+
+<h1>Listado de Personas</h1>
+
+<table border="1">
+
+    <tr>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Telefono</th>
+        <th>CategoriaId</th>
+    </tr>
+
+    <?php
+    foreach ($rs as $fila) { ?>
+        <tr>
+
+            <td><a href="persona-ficha.php?id=<?=$fila["p_id"]?>">
+                    <?php if(isset($fila["p_estrella"])) { ?>
+                        <img src="estrella.jpg" width="10" height="10">
+                    <?php } else ?>
+                    <?= $fila["p_nombre"] ?>
+                </a></td>
+
+
+            <td><a href="persona-ficha.php?id=<?=$fila["p_id"]?>"> <?=$fila["p_apellido"] ?> </a></td>
+            <td><a href="persona-ficha.php?id=<?=$fila["p_id"]?>"> <?=$fila["p_telefono"] ?> </a></td>
+            <td><a href="persona-ficha.php?id=<?=$fila["p_id"]?>"> <?=$fila["p_categoriaId"] ?> </a></td>
+            <td><a href="persona-eliminar.php?id=<?=$fila["p_id"]?>"> (X)                   </a></td>
+        </tr>
+    <?php } ?>
+
+</table>
+
+<br />
+
+<a href="persona-ficha.php?id=-1">Crear entrada</a>
+
+<br />
+<br />
+
+<a href="categoria-listado.php">Gestionar listado de Categorias</a>
+<br />
+<a href="persona-estrellas.php">Listado personas con estrellas</a>
+<br />
+<a href="persona-noEstrellas.php">Listado personas sin estrellas</a>
+
+</body>
+
+</html>
